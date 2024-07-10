dff397991bea445496a16b960749f88a91305274
Muhammad Khizar <79244644+khizr45@users.noreply.github.com>
2024-07-10 21:03:48 +0500

    corrections

--- src/components/subComponents/AboutUs.jsx
+++ src/components/subComponents/AboutUs.jsx
@@ -5,7 +5,7 @@ function AboutUs() {
     <div className='h-[80vh] aboutusHeight p-4'>
       <img src='aboutus.svg' className='ml-40 aboutusPic'/>
       <div className='flex justify-around items-center w-[98.7vw] aboutus'>
-        <p className='ml-28 font-poppins text-white font-normal mb-20 aboutusSvg w-[30vw] aboutuspara'>Bookworm is a comprehensive platform designed for book enthusiasts. It offers a seamless experience for buying, renting, and borrowing books from fellow users. Our application goes beyond transactions by fostering a lively community for book discussions, reviews, and recommendations. With transparent pricing and a focus on book condition, Bookworm ensures you make informed decisions.</p>
+        <p className='font-poppins text-white font-normal mb-20 aboutusSvg w-[30vw] aboutuspara'>Bookworm is a comprehensive platform designed for book enthusiasts. It offers a seamless experience for buying, renting, and borrowing books from fellow users. Our application goes beyond transactions by fostering a lively community for book discussions, reviews, and recommendations. With transparent pricing and a focus on book condition, Bookworm ensures you make informed decisions.</p>
         <div className='flex justify-between items-center w-[45%] mt-4 aboutus gap-8'>
             <div className='flex flex-col justify-start items-center w-[55%] h-[60vh] border-4 border-[#FFCE31] rounded-lg firstbox'>
                 <img src='visionlogo.svg' className='mt-4'/>
