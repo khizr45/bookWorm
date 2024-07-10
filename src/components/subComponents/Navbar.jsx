11f556a7ac43822c41e6bbf74deb0adcc7dcf5d2
Muhammad Khizar <79244644+khizr45@users.noreply.github.com>
2024-07-10 19:27:55 +0500

    responsiveness

--- src/components/subComponents/Navbar.jsx
+++ src/components/subComponents/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar({scroll}) {
     <div className='flex justify-between bg-[#3B3B3B] items-center h-[12vh]'>
       <img src='logo.PNG' className='h-[60%] ml-10 mt-4 logo'/>
       <div className='flex justify-center mt-4 navbarOpts'>
-        <h3 className='text-white font-poppins ml-8 cursor-pointer'>Home</h3>
-        <h3 className='text-white font-poppins ml-8 cursor-pointer' onClick={()=>{scroll('about')}}>About</h3>
-        <h3 className='text-white font-poppins ml-8 cursor-pointer' onClick={()=>{scroll('features')}}>Features</h3>
-        <h3 className='text-white font-poppins ml-8 cursor-pointer downloadNowButton' onClick={()=>{scroll('download')}}>Download Now</h3>
+        <h3 className='text-white font-poppins ml-8 cursor-pointer navbaropt'>Home</h3>
+        <h3 className='text-white font-poppins ml-8 cursor-pointer navbaropt' onClick={()=>{scroll('about')}}>About</h3>
+        <h3 className='text-white font-poppins ml-8 cursor-pointer navbaropt' onClick={()=>{scroll('features')}}>Features</h3>
+        <h3 className='text-white font-poppins ml-8 cursor-pointer downloadNowButton navbaropt' onClick={()=>{scroll('download')}}>Download Now</h3>
       </div>
       <div className='w-[20%]'></div>
     </div>
