67dca688c240311324ad3bf1b0462a6ab2920dc6
Muhammad Khizar <79244644+khizr45@users.noreply.github.com>
2024-07-10 17:51:57 +0500

    scroller setup

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Navbar from './subComponents/Navbar'
 import Hero from './subComponents/Hero'
 import AboutUs from './subComponents/AboutUs'
@@ -6,20 +6,33 @@ import KeyFeatures from './subComponents/KeyFeatures'
 import Footer from './subComponents/footer'
 
 function MainPage() {
+    const aboutRef = useRef(null)
+    const featureRef = useRef(null)
+    const downloadRef = useRef(null)
+
+     const scroller = (section) => {
+        if (section === 'about' && aboutRef.current) {
+            aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+          } else if (section === 'features' && featureRef.current) {
+            featureRef.current.scrollIntoView({ behavior: 'smooth' });
+          } else if (section === 'download' && downloadRef.current) {
+            downloadRef.current.scrollIntoView({ behavior: 'smooth' });
+          }
+       }
   return (
     <div className='flex flex-col'>
-      <Navbar/>
+      <Navbar scroll = {scroller}/>
       <div className='w-[98.7vw] h-[20vh] bg-[#3B3B3B] extra'>
 
       </div>
       <Hero/>
-      <div className='w-[98.7vw]  bg-[#C81243] flex items-center aboutus'>
+      <div className='w-[98.7vw]  bg-[#C81243] flex items-center aboutus' ref={aboutRef} id='about'>
         <AboutUs/>
       </div>
-      <div className='w-[98.7vw] h-[120vh] bg-[#3B3B3B] flex items-center featureHeight'>
+      <div className='w-[98.7vw] h-[120vh] bg-[#3B3B3B] flex items-center featureHeight' ref={featureRef} id='features'>
         <KeyFeatures/>
       </div>
-      <div className='w-[98.7vw] h-[100vh] bg-[#C81243] downloads'>
+      <div className='w-[98.7vw] h-[100vh] bg-[#C81243] downloads' ref={downloadRef} id='download'>
         <Footer/>
       </div>
     </div>
